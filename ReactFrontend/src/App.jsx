f81fe1c5eee48e83eb9d892a489507a7cf43570c
VBking1 <william.selanger.kraemer@gmail.com>
2024-05-28 11:57:49 +0200

    Revert "ip"
    This reverts commit a6c3861926da8baa0b245a03b47a14b5ac0b3de3.

--- ReactFrontend/src/App.jsx
+++ ReactFrontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://172.31.25.150:3001/api/data')     //henter data fra server
+    fetch('http://localhost:3001/api/data')     //henter data fra server
       .then(response => response.json())  //gjør om til json format
       .then(data => {
         setData(data);         
